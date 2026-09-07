281ea0c4e7ed9215409df7624d1ef0d93e46ab04
Thang Phan <thangjenny2002@gmail.com>
2026-09-07 20:10:46 +0700

    style(api): format books server

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-import express from "express"
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const PORT = 3001
+const PORT = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
 const books = [
   {
     id: 1,
     name: "Spiderman",
-    author: "Thang"
+    author: "Thang",
   },
   {
     id: 2,
     name: "Startwars",
-    author: "Ngoc"
-  }
-]
+    author: "Ngoc",
+  },
+];
 
 const authorize = (req, res, next) => {
-  const authorizationHeader = req.headers["authorization"]
-  const token = authorizationHeader?.split(" ")[1]
+  const authorizationHeader = req.headers["authorization"];
+  const token = authorizationHeader?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
       error: "missing_access_token",
-      message: "Authorization bearer token is required"
-    })
+      message: "Authorization bearer token is required",
+    });
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
     if (err) {
       return res.status(401).json({
         error: "invalid_access_token",
-        message: err.message
-      })
+        message: err.message,
+      });
     }
 
-    next()
-  })
-}
+    next();
+  });
+};
 
-app.get('/books', authorize, (req, res) => {
-  res.json({status: 'Success', data: { books } })
-})
+app.get("/books", authorize, (req, res) => {
+  res.json({ status: "Success", data: { books } });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`)
-})
+  console.log(`Server is listening on port: ${PORT}`);
+});
